9b519adf0baf90c401985dad08f6dccf78b48638
Ioana Colceag <ioana.kol1101@gmail.com>
2023-12-17 16:46:07 +0200

    Operatii CRUD clasa eveiment

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,13 +1,19 @@
 package main;
 
 import model.Database;
+import model.Eveniment;
 import model.Pachet;
+import model.enums.TipEveniment;
 
+import java.sql.Date;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.UUID;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         try {
             Database.connect();
 
@@ -25,6 +31,23 @@ public class Main {
             pachet.update();
             System.out.println(Pachet.readOne(pachet.getId()));
             System.out.println(Pachet.readMany());
+
+
+            DateFormat df = new SimpleDateFormat("MM-dd-yyyy");
+            Eveniment eveniment = new Eveniment(UUID.randomUUID(), TipEveniment.NUNTA, new Date(df.parse("12-10-2025").getTime()), "locatie 1");
+            eveniment.insert();
+            eveniment.setLocatie("locatie 2");
+            eveniment.update();
+            System.out.println(Eveniment.readOne(eveniment.getidEveniment()));
+            System.out.println(Eveniment.readMany());
+            eveniment.delete();
+
+            eveniment = new Eveniment(UUID.randomUUID(), TipEveniment.BOTEZ, new Date(df.parse("05-05-2024").getTime()), "locatie 3");
+            eveniment.insert();
+            eveniment.setLocatie("locatie 4");
+            eveniment.update();
+            System.out.println(Eveniment.readOne(eveniment.getidEveniment()));
+            System.out.println(Eveniment.readMany());
 
             Database.disconnect();
         } catch (SQLException e) {
